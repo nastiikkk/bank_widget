b0a5689de7182d5274455adf7d7973726e5b4a7f
Nastya <nastya@email.com>
2026-09-15 14:20:21 +0300

    add widget functions

---
+++ src/widget.py
@@ -0,0 +1,33 @@
+from src.masks import get_mask_account, get_mask_card_number
+
+
+def mask_account_card(card: str) -> str:
+    """Функция, которая возвращает строку с замаскированным номером"""
+    if not card:
+        return "введите строку"
+
+    first_num_card = None
+    for i in range(len(card)):
+        if card[i].isdigit():
+            first_num_card = i
+            break
+
+    if first_num_card is None:
+        return "некорретный ввод"
+
+    card_name = card[:first_num_card].strip()
+    card_number = card[first_num_card:].strip()
+
+    if not card_number.isdigit():
+        return "некорретный ввод"
+
+    is_account = False
+    if "Счет" in card:
+        is_account = True
+
+    if is_account:
+        result = get_mask_account(card_number)
+    else:
+        result = get_mask_card_number(card_number)
+
+    return f"{card_name} {result}"
