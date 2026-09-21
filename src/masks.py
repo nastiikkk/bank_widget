192eaaa4a99ba73340f7890f95937fec52cd4623
Анастасия <anastasia@MacBook-Pro-Anastasia.local>
2026-09-21 21:03:06 +0300

    fix masks validation

--- src/masks.py
+++ src/masks.py
@@ -1,8 +1,12 @@
 def get_mask_card_number(card_number: str) -> str:
     """Функция маскировки номера банковской карты"""
-    return f"{card_number[:4]} {card_number[4:6]}** **** {card_number[12:]}"
+    if len(card_number) == 16 and card_number.isdigit():
+        return f"{card_number[:4]} {card_number[4:6]}** **** {card_number[12:]}"
+    raise ValueError('Некорректный номер карты')
 
 
 def get_mask_account(account: str) -> str:
     """Функция маскировки номера банковского счета"""
-    return f"**{account[-4:]}"
+    if len(account) >= 4 and account.isdigit():
+        return f"**{account[-4:]}"
+    raise ValueError('Некорректный номер счета')
