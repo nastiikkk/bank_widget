03ad06306689eed3f8c63f9ff6c279919bba78ab
Анастасия <anastasia@MacBook-Pro-Anastasia.local>
2026-09-22 15:02:19 +0300

    processing validation fixes

--- src/processing.py
+++ src/processing.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+
+
 def filter_by_state(data: list[dict], state: str = "EXECUTED") -> list[dict]:
     """Функция, которая возвращает список словарей у которых ключ state
     соответствует указанному значению"""
@@ -12,5 +15,7 @@ def filter_by_state(data: list[dict], state: str = "EXECUTED") -> list[dict]:
 
 def sort_by_date(data: list[dict], reverse: bool = True) -> list[dict]:
     """Функция, которая сортирует список словарей по дате"""
+    for operation in data:
+        datetime.fromisoformat(operation["date"])
 
     return sorted(data, key=lambda x: x["date"], reverse=reverse)
