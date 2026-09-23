cc139a59ebedd39b872fbe0e1a7f3bb3d6120561
Анастасия <anastasia@MacBook-Pro-Anastasia.local>
2026-09-23 14:23:38 +0300

    add new functions

---
+++ src/generators.py
@@ -0,0 +1,23 @@
+from typing import Generator
+
+
+def filter_by_currency(
+    transactions: list[dict], code: str
+) -> Generator[dict, None, None]:
+    """Возвращает транзакции только с указанной валютой"""
+    for transaction in transactions:
+        if transaction["operationAmount"]["currency"]["code"] == code:
+            yield transaction
+
+
+def transaction_descriptions(transactions: list[dict]) -> Generator[str, None, None]:
+    """Возвращает описание операции"""
+    for transaction in transactions:
+        yield transaction["description"]
+
+
+def card_number_generator(start: int, stop: int) -> Generator[str, None, None]:
+    """Генерирует номера банковских карт"""
+    for number in range(start, stop + 1):
+        number_str = f"{number:016d}"
+        yield f"{number_str[:4]} {number_str[4:8]} {number_str[8:12]} {number_str[12:16]}"
